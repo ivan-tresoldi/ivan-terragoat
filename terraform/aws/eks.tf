92e68b9dc6c4f86c21e9e2fafa6898b9fa8fb2a4
Ivan Tresoldi <johndoe@example.com>
2022-01-25 14:53:35 +0100

    S3 SSE set on Bucker "data"

--- terraform/aws/eks.tf
+++ terraform/aws/eks.tf
@@ -41,6 +41,7 @@ resource aws_iam_role_policy_attachment "policy_attachment-AmazonEKSServicePolic
 }
 
 resource aws_vpc "eks_vpc" {
+	# checkov:skip=BC_AWS_LOGGING_9: VPC Flow Logs not needed here
   cidr_block           = "10.10.0.0/16"
   enable_dns_hostnames = true
   enable_dns_support   = true
