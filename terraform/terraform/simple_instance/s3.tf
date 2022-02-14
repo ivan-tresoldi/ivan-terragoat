f5cf5f814307aee8484477355ae1281fd4adb494
ivan-tresoldi <67233434+ivan-tresoldi@users.noreply.github.com>
2022-02-14 11:20:22 +0100

    Create s3.tf

---
+++ terraform/terraform/simple_instance/s3.tf
@@ -0,0 +1,12 @@
+provider "aws" {
+  region = "eu-central-1"
+}
+
+resource "aws_s3_bucket" "docking_bay" {
+  bucket_prefix = "docking-bay-storage-"
+
+  tags = {
+    Name                 = "Docking Bay"
+    Environment          = "Dev"
+  }
+}
